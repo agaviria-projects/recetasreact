0e180662fea93d1f17a1d15c2a2a9a437821c1d9
Alejandro <agaviria1408@gmail.com>
2025-04-05 15:50:56 -0500

    Se crea componente para las redes sociales

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,26 @@
-import "./../assets/estilos.css";
 import React from 'react';
+import Redes from "./redes";
+import "./../assets/estilos.css";
 
 export default function Header() {
   return (
-    <div className="header-personalizado">
-      <img 
-        className="logo" 
-        src="https://img.freepik.com/vector-premium/logotipo-chef-hombre-logotipo-restaurantes-logotipo-comida-rapida_663736-303.jpg?w=740" 
-        alt="Logo"
-      />
-      <h1>El Fogón Urbano</h1>
-      <p>El fuego lo encendemos nosotros, el hambre la traes vos</p> 
+    <div className="header-container">
+      <div className="redes-superior">
+        <Redes />
+      </div>
+
+      <div className="header-personalizado">
+        <img 
+          className="logo" 
+          src="https://img.freepik.com/vector-premium/logotipo-chef-hombre-logotipo-restaurantes-logotipo-comida-rapida_663736-303.jpg?w=740" 
+          alt="Logo"
+        />
+        <h1>El Fogón Urbano</h1>
+        <p>El fuego lo encendemos nosotros, el hambre la traes vos</p> 
+      </div>
     </div>
   );
 }
+
 
 
