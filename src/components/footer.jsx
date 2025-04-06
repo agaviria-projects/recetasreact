0a6c29061a5876d4790e44b3216d5624ec4657de
Alejandro <agaviria1408@gmail.com>
2025-04-06 14:19:57 -0500

    Se modifica el mensaje del footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="mt-5 p-4 bg-dark text-white text-center">
-        <p>@Todos los derechos reservados - Recetas App Gaviria - 2025</p>
+        <p>© 2025 El Fogón Urbano | Sabores que cuentan historias. Desarrollado por Gaviria. Todos los derechos reservados.</p>
     </div>
   )
 }
