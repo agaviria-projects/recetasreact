518ac1d505235296a603431682536790113574a2
Alejandro <agaviria1408@gmail.com>
2025-04-06 10:05:39 -0500

    Añadi una imagen al componente Aside.jsx

--- src/components/aside.jsx
+++ src/components/aside.jsx
@@ -5,8 +5,10 @@ export default function Aside() {
     <div className="col-sm-4">
         <h2>About Me</h2>
         <h5>Photo of me:</h5>
-        <div className="fakeimg">Fake Image</div>
-        <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+        <div className="fakeimg">
+            <img className="img-fluid" src={"https://img.freepik.com/foto-gratis/parrilla-variedad-carnes-ella_188544-8372.jpg?t=st=1743950937~exp=1743954537~hmac=b903d3b9a532849455c940fc9def0bd7be5fabd5e7687f3ae2e8dd56a2852dd6&w=1060"} alt="" />
+        </div>
+        <p className="mt-3">Una parrilla con una variedad de carnes en ella</p>
         <h3 className="mt-4">Some Links</h3>
         <p>Lorem ipsum dolor sit ame.</p>
         <ul className="nav nav-pills flex-column">
